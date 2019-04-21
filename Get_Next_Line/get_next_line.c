c57b309b0870a663a2387ec4d69e39afb67031b1
Leyla Arlyne <larlyne@e6r4p8.21-school.ru>
2019-04-21 10:38:53 +0300

    Respect The Norm

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: larlyne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 12:25:34 by larlyne           #+#    #+#             */
-/*   Updated: 2019/04/20 12:25:44 by larlyne          ###   ########.fr       */
+/*   Updated: 2019/04/21 10:16:29 by larlyne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,15 @@ static int		get_next_line_get(t_fd_buffer *info, char *new_line_pos,
 	char	*temp;
 	size_t	pos;
 
-	if (new_line_pos == NULL)
-			pos = info->length;
-	else
-		pos = new_line_pos - info->buff;
+	pos = (new_line_pos == NULL) ? info->length : new_line_pos - info->buff;
 	if ((*line = ft_strnew(pos)) == NULL)
-			return (-1);
+		return (-1);
 	ft_memcpy(*line, info->buff, pos);
 	if (pos == info->length)
 	{
 		free(info->buff);
-		info->buff = ft_strnew(1);
+		if ((info->buff = ft_strnew(1)) == NULL)
+			return (-1);
 		info->length = 0;
 	}
 	else
--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: larlyne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 12:25:57 by larlyne           #+#    #+#             */
-/*   Updated: 2019/04/20 12:26:04 by larlyne          ###   ########.fr       */
+/*   Updated: 2019/04/21 10:28:21 by larlyne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 512
+# define BUFF_SIZE 1024
 # include <string.h>
 
 typedef struct	s_fd_buffer
