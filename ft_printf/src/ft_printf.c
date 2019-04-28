c568acf50f2eda1ea1af1cfc082f9e88c8e057fe
PofigistVip <pruskalev@gmail.com>
2019-04-28 20:57:46 +0300

    ft_printf.c created

---
+++ ft_printf/src/ft_printf.c
@@ -0,0 +1,37 @@
+
+#include <stdarg.h>
+#include "ft_printf.h"
+
+int is_conversion_specifier(char c)
+{
+	return (c == 'd' || c == 'i' || c == 'o' || c == 'u'
+		|| c == 'x' || c == 'X' || c == 'e' || c == 'E'
+		|| c == 'f' || c == 'F' || c == 'g' || c == 'G'
+		|| c == 'a' || c == 'A' || c == 'c' || c == 's'
+		|| c == 'C' || c == 'S' || c == '%');
+}
+
+int		ft_printf(const char *format, ...)
+{
+	int		i;
+	int		n;
+
+	i = 0;
+	n = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (n != 0)
+				ft_putnchar(format + i - n - 1, n);
+			n = 0;
+			while (!is_conversion_specifier(format[++i]))
+			{
+				
+			}
+		}
+		else
+			++n;
+		++i;
+	}
+}
