790db3d367fc719f99d38cc06b65479802f73988
PofigistVip <pruskalev@gmail.com>
2019-05-01 15:53:25 +0300

    Makefile and libft added

---
+++ ft_printf/lib/libft/ft_llist_clear.c
@@ -0,0 +1,12 @@
+
+
+#include <string.h>
+#include "libft.h"
+
+void	ft_llist_clear(t_llist *llist, void (*cont_destroy)(void*))
+{
+	if (llist == NULL || cont_destroy == NULL)
+		return ;
+	while (llist->count > 0)
+		ft_llist_remove(llist, 0, cont_destroy);
+}
