3075dd095f56d5515174154c6d1144abe5fb6e4a
PofigistVip <pruskalev@gmail.com>
2019-05-04 17:22:11 +0300

    append tests

--- mytests/libft/main.c
+++ mytests/libft/main.c
@@ -1,5 +1,18 @@
 
+#include <stdlib.h>
 #include "libft.h"
+
+
+#define CHECK(lstr, cmp) \
+	ft_print(lstr); \
+	if (strcmp(lstr->str, cmp) == 0) { \
+		ft_setforeground(2); \
+		ft_putstr(" [ OK ]");  } \
+	else  { \
+		ft_setforeground(1); \
+		ft_putstr(" [FAIL]");  } \
+	ft_setdefault_cols(1); \
+	ft_lstr_destroy(&lstr);
 
 void	ft_print(t_lstr *lstr)
 {
@@ -17,20 +30,56 @@ int		ft_lstr_tests(void)
 {
 	t_lstr	*lstr;
 
-	ft_putstr("Empty lstr: ");
+	ft_putstr("===CREATING===\n");
+	ft_putstr(" Empty lstr: ");
 	lstr = ft_lstr_new_empty();
-	ft_print(lstr);
-	ft_lstr_destroy(&lstr);
+	CHECK(lstr, "");
 
-	ft_putstr("\nwww str: ");
+	ft_putstr("\n www str: ");
 	lstr = ft_lstr_new('w', 3);
-	ft_print(lstr);
-	ft_lstr_destroy(&lstr);
+	CHECK(lstr, "www");
 
-	ft_putstr("\nRaw 'abcd': ");
+	ft_putstr("\n Raw 'abcd': ");
 	lstr = ft_lstr_new_raw("abcd");
-	ft_print(lstr);
-	ft_lstr_destroy(&lstr);
+	CHECK(lstr, "abcd");
+
+	ft_putstr("\n===INSERTING===");
+	ft_putstr("\n ==CHAR==\n");
+	ft_putstr(" Insert to 0 'a' in empty: ");
+	lstr = ft_lstr_new_empty();
+	ft_lstr_insert_c(lstr, 'a', 0);
+	CHECK(lstr, "a");
+
+	ft_putstr("\n Insert to 0 'a' in 'bcd': ");
+	lstr = ft_lstr_new_raw("bcd");
+	ft_lstr_insert_c(lstr, 'a', 0);
+	CHECK(lstr, "abcd");
+
+	ft_putstr("\n Insert to 2 'c' in 'abd': ");
+	lstr = ft_lstr_new_raw("abd");
+	ft_lstr_insert_c(lstr, 'c', 2);
+	CHECK(lstr, "abcd");
+
+	ft_putstr("\n Insert to 3 'd' in 'abc': ");
+	lstr = ft_lstr_new_raw("abc");
+	ft_lstr_insert_c(lstr, 'd', 3);
+	CHECK(lstr, "abcd");
+
+	ft_putstr("\n ==STRING==\n");
+	ft_putstr(" Insert 'abcd' in empty: ");
+	lstr = ft_lstr_new_empty();
+	ft_lstr_insert_s(lstr, "abcd", 0);
+	CHECK(lstr, "abcd");
+
+	ft_putstr("\n Insert to 0 'ab' in 'cd': ");
+	lstr = ft_lstr_new_raw("cd");
+	ft_lstr_insert_s(lstr, "ab", 0);
+	CHECK(lstr, "abcd");
+
+	ft_putstr("\n Insert to 2 'cd' in 'ab': ");
+	lstr = ft_lstr_new_raw("ab");
+	ft_lstr_insert_s(lstr, "cd", 2);
+	CHECK(lstr, "abcd");
 
 	ft_putchar('\n');
 }
