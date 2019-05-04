529bbe2f0a3be011c70b8ca48c29508b6aae9a7a
PofigistVip <pruskalev@gmail.com>
2019-05-04 16:15:12 +0300

    lstr create func tests

--- mytests/libft/main.c
+++ mytests/libft/main.c
@@ -3,9 +3,14 @@
 
 void	ft_print(t_lstr *lstr)
 {
+	ft_putchar('\t');
 	ft_putchar('\'');
 	ft_lstr_put(lstr);
 	ft_putchar('\'');
+	ft_putstr(" \tLength: ");
+	ft_putnbr(lstr->length);
+	ft_putstr(" \tCapacity: ");
+	ft_putnbr(lstr->capacity);
 }
 
 int		ft_lstr_tests(void)
@@ -13,7 +18,7 @@ int		ft_lstr_tests(void)
 	t_lstr	*lstr;
 
 	ft_putstr("Empty lstr: ");
-	lstr = ft_lstr_empty();
+	lstr = ft_lstr_new_empty();
 	ft_print(lstr);
 	ft_lstr_destroy(&lstr);
 
@@ -22,17 +27,20 @@ int		ft_lstr_tests(void)
 	ft_print(lstr);
 	ft_lstr_destroy(&lstr);
 
-	ft_putstr("\nRaw from 'abcd': ");
-	lstr = ft_lstr_from_raw("abcd");
+	ft_putstr("\nRaw 'abcd': ");
+	lstr = ft_lstr_new_raw("abcd");
 	ft_print(lstr);
 	ft_lstr_destroy(&lstr);
 
-	
+	ft_putchar('\n');
 }
 
 int		main(void)
 {
-	ft_putstr("Larlyne's Strings Test (LSTR):\n");
+	ft_setcolors(0, 7);
+	ft_putstr("Larlyne's Strings Test (LSTR):");
+	ft_setdefault_cols(1);
+	ft_putchar('\n');
 	ft_lstr_tests();
 
 	return (0);
