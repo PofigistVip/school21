bdc71a9ff257f07c6faf5f6c680897bc97d52ca0
PofigistVip <pruskalev@gmail.com>
2019-05-05 09:32:43 +0300

    ft_uitoa_base tests added

--- mytests/libft/main.c
+++ mytests/libft/main.c
@@ -3,6 +3,17 @@
 #include "libft.h"
 
 
+#define CHECK_STR(str, cmp) \
+	ft_putstr(str); \
+	if (strcmp(str, cmp) == 0) { \
+		ft_setforeground(2); \
+		ft_putstr(" [ OK ]");  } \
+	else  { \
+		ft_setforeground(1); \
+		ft_putstr(" [FAIL]");  } \
+	ft_setdefault_cols(1); \
+	free(str);
+
 #define CHECK(lstr, cmp) \
 	ft_print(lstr); \
 	if (strcmp(lstr->str, cmp) == 0) { \
@@ -26,7 +37,100 @@ void	ft_print(t_lstr *lstr)
 	ft_putnbr(lstr->capacity);
 }
 
-int		ft_lstr_tests(void)
+void	ft_utility_tests(void)
+{
+	char	*num;
+
+	ft_putstr("===FT_UITOA_BASE===");
+	ft_putstr("\n ==ZERO==\n");
+	ft_putstr("  0 dec to bin:\t\t ");
+	num = ft_uitoa_base(0, 2);
+	CHECK_STR(num, "0");
+
+	ft_putstr("\n  0 dec to oct:\t\t ");
+	num = ft_uitoa_base(0, 8);
+	CHECK_STR(num, "0");
+
+	ft_putstr("\n  0 dec to dec:\t\t ");
+	num = ft_uitoa_base(0, 10);
+	CHECK_STR(num, "0");
+
+	ft_putstr("\n  0 dec to hex:\t\t ");
+	num = ft_uitoa_base(0, 16);
+	CHECK_STR(num, "0");
+
+	ft_putstr("\n ==BASE-1==\n");
+	ft_putstr("  1 dec to bin:\t\t ");
+	num = ft_uitoa_base(1, 2);
+	CHECK_STR(num, "1");
+
+	ft_putstr("\n  7 dec to oct:\t\t ");
+	num = ft_uitoa_base(7, 8);
+	CHECK_STR(num, "7");
+
+	ft_putstr("\n  9 dec to dec:\t\t ");
+	num = ft_uitoa_base(9, 10);
+	CHECK_STR(num, "9");
+
+	ft_putstr("\n  15 dec to hex:\t ");
+	num = ft_uitoa_base(15, 16);
+	CHECK_STR(num, "f");
+
+	ft_putstr("\n ==BASE==\n");
+	ft_putstr("  2 dec to bin:\t\t ");
+	num = ft_uitoa_base(2, 2);
+	CHECK_STR(num, "10");
+
+	ft_putstr("\n  8 dec to oct:\t\t ");
+	num = ft_uitoa_base(8, 8);
+	CHECK_STR(num, "10");
+
+	ft_putstr("\n  10 dec to dec:\t ");
+	num = ft_uitoa_base(10, 10);
+	CHECK_STR(num, "10");
+
+	ft_putstr("\n  16 dec to hex:\t ");
+	num = ft_uitoa_base(16, 16);
+	CHECK_STR(num, "10");
+
+	ft_putstr("\n ==256==\n");
+	ft_putstr("  256 dec to bin:\t ");
+	num = ft_uitoa_base(256, 2);
+	CHECK_STR(num, "100000000");
+
+	ft_putstr("\n  256 dec to oct:\t ");
+	num = ft_uitoa_base(256, 8);
+	CHECK_STR(num, "400");
+
+	ft_putstr("\n  256 dec to dec:\t ");
+	num = ft_uitoa_base(256, 10);
+	CHECK_STR(num, "256");
+
+	ft_putstr("\n  256 dec to hex:\t ");
+	num = ft_uitoa_base(256, 16);
+	CHECK_STR(num, "100");
+
+	ft_putstr("\n ==125==\n");
+	ft_putstr("  125 dec to bin:\t ");
+	num = ft_uitoa_base(125, 2);
+	CHECK_STR(num, "1111101");
+
+	ft_putstr("\n  125 dec to oct:\t ");
+	num = ft_uitoa_base(125, 8);
+	CHECK_STR(num, "175");
+
+	ft_putstr("\n  125 dec to dec:\t ");
+	num = ft_uitoa_base(125, 10);
+	CHECK_STR(num, "125");
+
+	ft_putstr("\n  125 dec to hex:\t ");
+	num = ft_uitoa_base(125, 16);
+	CHECK_STR(num, "7d");
+
+	ft_putchar('\n');
+}
+
+void	ft_lstr_tests(void)
 {
 	t_lstr	*lstr;
 
@@ -98,6 +202,11 @@ int		ft_lstr_tests(void)
 
 	ft_putstr("\n===PLACE===");
 	ft_putstr("\n ==CHAR==");
+	ft_putstr("\n  Place 'c' to 3 in 'abdd': ");
+	lstr = ft_lstr_new_copy("abdd");
+	ft_lstr_place_c(lstr, 'c', 1, 2);
+	CHECK(lstr, "abcd");
+
 	ft_putstr("\n  Place 2'a' to 0 in 'bbbb': ");
 	lstr = ft_lstr_new_copy("bbbb");
 	ft_lstr_place_c(lstr, 'a', 2, 0);
@@ -138,6 +247,12 @@ int		ft_lstr_tests(void)
 
 int		main(void)
 {
+	ft_setcolors(0, 7);
+	ft_putstr("Larlyne's Utility Test:");
+	ft_setdefault_cols(1);
+	ft_putchar('\n');
+	ft_utility_tests();
+
 	ft_setcolors(0, 7);
 	ft_putstr("Larlyne's Strings Test (LSTR):");
 	ft_setdefault_cols(1);
